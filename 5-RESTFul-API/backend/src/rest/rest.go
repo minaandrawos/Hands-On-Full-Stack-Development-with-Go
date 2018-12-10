825cc64f84200ab70c76e4774fdb421bb8d0c611
minaandrawos <mina.andrews@gmail.com>
2018-12-09 16:24:28 -0800

    Update rest.go

--- 5-RESTFul-API/backend/src/rest/rest.go
+++ 5-RESTFul-API/backend/src/rest/rest.go
@@ -5,22 +5,36 @@ import (
 )
 
 func RunAPI(address string) error {
+	//Get gin's default engine
 	r := gin.Default()
+	//load homepage
+	r.GET("/", h.GetMainPage)
 	//get products
-	//get promos
-	//post user sign in
-	//get user orders
-	//post user sign out
-	//post purchase charge
-	h, _ := NewHandler()
 	r.GET("/products", h.GetProducts)
+	//get promos
 	r.GET("/promos", h.GetPromos)
+	/*
+		//post user sign in
+		r.POST("/user/signin", h.SignIn)
+		//post user sign out
+		r.POST("/user/:id/signout", h.SignOut)
+		//get user orders
+		r.GET("/user/:id/orders", h.GetOrders)
+		//post purchase charge
+		r.POST("/user/charge", h.Charge)
+	*/
+
 	userGroup := r.Group("/user")
 	{
-		userGroup.POST("/signin", h.SignIn)
 		userGroup.POST("/:id/signout", h.SignOut)
 		userGroup.GET("/:id/orders", h.GetOrders)
-		userGroup.POST("/charge", h.Charge)
+	}
+
+	usersGroup := r.Group("/users")
+	{
+		usersGroup.POST("/charge", h.Charge)
+		usersGroup.POST("/signin", h.SignIn)
+		usersGroup.POST("", h.AddUser)
 	}
 	return r.Run(address)
 }
